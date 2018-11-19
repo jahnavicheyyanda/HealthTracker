64e856ba2bc484f1c9b2e1722607321e7006a95a
Z.Ece <zece.ergin@gmail.com>
2018-11-20 00:35:17 +0100

    created all screens

---
+++ app/src/main/java/com/example/zece/healthtracker/connection_check.java
@@ -0,0 +1,13 @@
+package com.example.zece.healthtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class connection_check extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connection_check);
+    }
+}
---
+++ app/src/main/java/com/example/zece/healthtracker/saved_wave.java
@@ -0,0 +1,13 @@
+package com.example.zece.healthtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class saved_wave extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_wave);
+    }
+}
