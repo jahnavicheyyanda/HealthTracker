a6771ec4669d4b54ed4258d077f5cf83daf27c2d
Z.Ece <zece.ergin@gmail.com>
2018-11-21 14:32:55 +0100

    button activities completed in order to go to next page

--- app/src/main/java/com/example/zece/healthtracker/patient_data.java
+++ app/src/main/java/com/example/zece/healthtracker/patient_data.java
@@ -1,7 +1,10 @@
 package com.example.zece.healthtracker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class patient_data extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class patient_data extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_data);
+
+        final Button save_button = (Button) findViewById(R.id.save_button);
+
+        save_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                save_button();
+            }
+        });
+    }
+
+    public void save_button(){
+        Intent intent_save = new Intent(this, files_page.class);
     }
 }
--- app/src/main/java/com/example/zece/healthtracker/recorded_wave.java
+++ app/src/main/java/com/example/zece/healthtracker/recorded_wave.java
@@ -1,7 +1,10 @@
 package com.example.zece.healthtracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class recorded_wave extends AppCompatActivity {
 
@@ -9,5 +12,33 @@ public class recorded_wave extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recorded_wave);
+
+        Button next_button = (Button) findViewById(R.id.next_button);
+        Button cancel_button = (Button) findViewById(R.id.cancel_button);
+
+        next_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                next_button();
+            }
+        });
+
+        cancel_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancel_button();
+            }
+        });
+    }
+
+    public void next_button() {
+        Intent intent_next = new Intent(this, patient_data.class);
+                startActivity(intent_next);
+    }
+
+    public void cancel_button(){
+        Intent intent_cancel = new Intent(this, recording_wave.class);
+                startActivity(intent_cancel);
+
     }
 }
