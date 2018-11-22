f8cca5cedc22d922607c062fcb6873063c5c7f17
jahnavicheyyanda <jahnavi.ct1@gmail.com>
2018-11-22 11:18:16 +0100

    deleted connection page and added contents to main activity

--- app/src/main/java/com/example/zece/healthtracker/MainActivity.java
+++ app/src/main/java/com/example/zece/healthtracker/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.zece.healthtracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button next_button2 = findViewById(R.id.next_button_home);
+
+        next_button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recording_wave();
+            }
+        });
     }
+    public void recording_wave() {
+        Intent intent = new Intent(this, recording_wave.class);
+        startActivity(intent);
+    }
+
+
+
 }
