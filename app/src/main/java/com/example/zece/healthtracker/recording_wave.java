4efe2c3f6b6058c975891d0fd13bfba8f1ccddc2
jahnavicheyyanda <jahnavi.ct1@gmail.com>
2018-12-13 11:48:32 +0100

    Theme and button color

--- app/src/main/java/com/example/zece/healthtracker/recording_wave.java
+++ app/src/main/java/com/example/zece/healthtracker/recording_wave.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.media.audiofx.Visualizer;
 
 public class recording_wave extends AppCompatActivity {
 
