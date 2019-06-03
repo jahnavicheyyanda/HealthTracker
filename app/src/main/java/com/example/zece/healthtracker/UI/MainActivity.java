9d645282e94a190063f656c481fc9788f2d6b6d0
jahnavicheyyanda <jahnavi.ct1@gmail.com>
2019-06-03 12:01:16 +0200

    Added exception for record button

--- app/src/main/java/com/example/zece/healthtracker/UI/MainActivity.java
+++ app/src/main/java/com/example/zece/healthtracker/UI/MainActivity.java
@@ -277,10 +277,18 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 if(file.exists()){
                     file.delete();
                 }
+                TextView text_status = findViewById(R.id.text_bluetooth_connecton_status);
+                if (mmSocket.isConnected()) {
+                    //text_status.setText("You are Connected");
+                    byte[] num = "1".getBytes();
+                    mConnectedThread.write(num);
+                }
+                else {
+                    text_status.setText("Please select a bluetooth device");
+                }
 
-
-                byte[] num = "1".getBytes();
-                mConnectedThread.write(num);
+                //byte[] num = "1".getBytes();
+                //mConnectedThread.write(num);
                 try {
                     mmSocket.close();
                 } catch (IOException e) {
